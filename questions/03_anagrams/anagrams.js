8ed66a919e33a292c955d23ba2a787181c0cfedd
Kwon <sanghyukkwon90@gmail.com>
2020-04-29 18:58:20 -0400

    done

--- questions/03_anagrams/anagrams.js
+++ questions/03_anagrams/anagrams.js
@@ -2,7 +2,7 @@
 // Return a boolean
 
 const checkAnagrams = (str1, str2) => {
-  return str1.split("").sort() == str2.split("").sort();
+  return str1.split("").sort().join("") == str2.split("").sort().join("");
 };
 
 module.exports = { checkAnagrams };
