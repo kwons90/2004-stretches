db2f865e218424d2f56f4e95f489cc7cafa46d31
Kwon <sanghyukkwon90@gmail.com>
2020-05-16 04:01:43 -0400

    floyds done

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,32 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+    if(n ==0 || n ==undefined) {
+        return ''
+    }
+        num = 0
+    for (let i = 0; i < n; i++) {
+        num += (i + 1)
+    }
+    f = 1
+    arr = []
+    for (let i = 0; i < num; i++) {
+        subArr = []
+        for (let j = 0; j < f; j++) {
+            subArr.push(i + 1)
+            i += 1
+        }
+        i -= 1
+        f += 1
+        arr.push(subArr)
+    }
+    str = arr[0].join('')
+    console.log(arr[0])
+    for (let i = 1; i < arr.length; i++) {
+        str = str + '\n' + arr[i].join('')
+    }
+    return str
+};
 
 module.exports = { floydsTriangle };
