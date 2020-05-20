370bb447abc40b82f4e65a3a9706e8ead97c34d3
Kwon <sanghyukkwon90@gmail.com>
2020-05-19 22:41:51 -0400

    cached finished with EC

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,30 @@
 // see test specs
-
-function cache() {
-  // YOUR CODE
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
 
-module.exports = { cache };
+function cache(func) {
+  if (typeof (func) != 'function') {
+    throw new Error('Input must be a function.')
+  }
+  cached = []
+  return function () {
+    values = Object.values(arguments)
+    for (i in cached) {
+      if (arraysEqual(cached[i][0], values)) {
+        return cached[i][1]
+      }
+    }
+    const output = func(...Object.values(arguments))
+    cached.push([values,output])
+    return output
+  }
+}
+
+  module.exports = { cache };
