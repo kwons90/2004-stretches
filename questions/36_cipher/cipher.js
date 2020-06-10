25341275f9ef5a50329a3c3bb93c8d35f36380e1
Kwon <sanghyukkwon90@gmail.com>
2020-06-10 18:43:18 -0400

    cipher done incl. bonus

--- questions/36_cipher/cipher.js
+++ questions/36_cipher/cipher.js
@@ -1,14 +1,22 @@
 const encrypt = (str) => {
   let returnArr = []
-  strL = str.split('')
-  console.log(strL)
-  returnArr.push(strL[0].charCodeAt(0))
-  returnArr.push(strL.pop())
-  let slicedStrL = strL.slice(2)
-  returnArr = [...returnArr,...slicedStrL]
-  console.log('returnArr is ',returnArr)
-  returnArr.push(strL[1])
-  return returnArr.join('')
+  let strL = str.split(' ')
+  const encryptWord = function(word) {
+    let wordL = word.split('')
+    let wordArr= []
+    wordArr.push(wordL[0].charCodeAt(0))
+    wordArr.push(wordL.pop())
+    let slicedWord = wordL.slice(2)
+    wordArr = [...wordArr,...slicedWord]
+    wordArr.push(wordL[1])
+    return wordArr.join('')
+  }
+  strL.forEach(currV => {
+    // console.log('word is ',currV)
+    returnArr.push(encryptWord(currV))
+  })
+  console.log(returnArr.join(''))
+  return returnArr.join(' ')
 };
 
 module.exports = { encrypt };
