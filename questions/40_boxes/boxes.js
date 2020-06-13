2f15ba4f22e1c3b726c48bcf1dc3c4890f29c65e
Kwon <sanghyukkwon90@gmail.com>
2020-06-13 22:29:03 +0900

    nothing works

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -5,11 +5,23 @@ class Box {
         this.color = color;
         this.capacity = capacity;
         this.contents = []
+        this.length = 0
+        this.colors = [this.color]
     }
     pack(color, length) {
+        if(this.length + length > this.capacity) {
+            throw new Error('nope')
+        }
+        this.length += length
         const box = new Box(color, length)
         this.contents.push(box)
+        this.colors.push(box.color)
     }
+
+    unpack() {
+        return this.colors         
+    }
+
 }
 
 
