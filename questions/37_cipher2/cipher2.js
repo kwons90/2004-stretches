627a318a38fd0108328d1d64f87a7e0b5afd3385
Kwon <sanghyukkwon90@gmail.com>
2020-06-15 08:41:22 +0900

    cipher2 done

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,15 +1,15 @@
 const decypher = (str) => {
   let strL = str.split(' ')
-  console.log(strL)
+  // console.log(strL)
   let returnArr = []
   const decypherWord = function(word) {
     let wordL = word.split('')
-    console.log('wordL is ',wordl)
+    // console.log('wordL is ',wordL)
     let wordArr= []
     let firstLetter = []
     let j = 0
     for(let i = 0;i<wordL.length;i++) {
-      if(parseInt(wordL[i]) == NaN) {
+      if(isNaN(parseInt(wordL[i]))) {
         break
       }
       else{
@@ -21,14 +21,14 @@ const decypher = (str) => {
     wordArr.push(wordL.pop())
     let slicedWord = wordL.slice(j+1)
     wordArr = [...wordArr,...slicedWord]
-    wordArr.push(wordL[1])
+    wordArr.push(wordL[j])
     return wordArr.join('')
   }
   strL.forEach(currV => {
     // console.log('word is ',currV)
     returnArr.push(decypherWord(currV))
   })
-  console.log(returnArr.join(''))
+  // console.log(returnArr.join(''))
   return returnArr.join(' ')
 };
 module.exports = { decypher };
